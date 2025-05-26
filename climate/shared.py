2e30f1bbb53ac826928596bb35f85f882961c11b
DaMitti <da.mitti@posteo.de>
2025-05-26 07:13:37 +0000

    bugfix: reset before setting

--- climate/shared.py
+++ climate/shared.py
@@ -301,7 +301,7 @@ class NormalizationMixin:
         )
 
         # Set index
-        df_indicator = df_indicator.set_index(["pgid", "year", "quarter"]).sort_index()
+        df_indicator = df_indicator.reset_index().set_index(["pgid", "year", "quarter"]).sort_index()
         df_indicator = df_indicator.loc[
             (slice(None), slice(start_year, None), slice(None)), slice(None)
         ]
