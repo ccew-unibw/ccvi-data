919c9510beb27c910aec8501b9ad93ab6879b2e1
DaMitti <da.mitti@posteo.de>
2025-07-01 12:41:55 +0000

    move spei-specific functions into spei

--- base/datasets/spei.py
+++ base/datasets/spei.py
@@ -19,8 +19,6 @@ from joblib import Parallel, delayed
 
 from datetime import timedelta
 
-from utils.ee import get_grid_chunks, fc_to_dict
-
 import duckdb
 
 import subprocess
@@ -46,6 +44,32 @@ except:
     print(
         "R not found. R related commands will not work. Execute them in a Docker container instead!"
     )
+
+
+def fc_to_dict(fc: ee.FeatureCollection) -> ee.Dictionary:
+    prop_names = fc.first().propertyNames()
+    prop_lists = fc.reduceColumns(
+        reducer=ee.Reducer.toList().repeat(prop_names.size()), selectors=prop_names
+    ).get("list")
+    return ee.Dictionary.fromLists(prop_names, prop_lists)
+
+
+def get_grid_chunks(client: GEEClient, chunks: int = 10) -> list:
+    """Return a list of tuples with lower and upper bounds of roughly equally sized chunks."""
+    pgids = client.asset.aggregate_array("pgid").getInfo()
+    pgids.sort()
+    chunk_size = len(pgids) // chunks
+    result = []
+    for i in range(chunks):
+        lower = i * chunk_size
+        upper = (i + 1) * chunk_size
+        if i == chunks - 1:
+            chunked_data = pgids[lower:]
+            result.append((chunked_data[0], chunked_data[-1]))
+        else:
+            chunked_data = pgids[lower:upper]
+            result.append((chunked_data[0], chunked_data[-1]))
+    return result
 
 
 def create_spei_mask(client: GEEClient, storage: str, force: bool = False):
