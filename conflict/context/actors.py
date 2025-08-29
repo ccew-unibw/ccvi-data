990eacbe0b25624d3a402958616ab5a648d2a506
DaMitti <da.mitti@posteo.de>
2025-08-29 14:28:32 +0000

    add per population to conflict actors

--- conflict/context/actors.py
+++ conflict/context/actors.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 
 from base.datasets import UCDPData
+from base.datasets.wpp import WPPData
 from base.objects import Indicator, ConfigParser, GlobalBaseGrid
 from conflict.shared import NormalizationMixin
+from utils.data_processing import default_impute
 
 
 class ConContextActors(Indicator, NormalizationMixin):
@@ -17,13 +19,16 @@ class ConContextActors(Indicator, NormalizationMixin):
     ):
         """Params defining indicator's place in index set to designed hierarchy by default"""
         self.ucdp = UCDPData(config=config)
+        self.wpp = WPPData(config=config)
         super().__init__(pillar=pillar, dim=dim, id=id, config=config, grid=grid)
 
-    def load_data(self) -> pd.DataFrame:
+    def load_data(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         df_ucdp = self.ucdp.load_data()
-        return df_ucdp
+        df_wpp = self.wpp.load_data()
+        return df_ucdp, df_wpp
 
-    def preprocess_data(self, df_ucdp: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def preprocess_data(self, dfs_input: tuple[pd.DataFrame, pd.DataFrame]) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        df_ucdp, df_wpp = dfs_input
         df_ucdp = self.ucdp.preprocess_data(df_ucdp, self.grid)
         country_stats = df_ucdp.groupby(["iso3", "year", "quarter"])[["event_count", "best"]].sum()
         # single entry per actor
@@ -35,10 +40,17 @@ class ConContextActors(Indicator, NormalizationMixin):
         # cleaning: drop civilians (only victims) and unclear actors
         grouped_base = grouped_base.loc[grouped_base.actor.str.lower() != "civilians"]
         grouped_base = grouped_base.loc[~grouped_base.actor.str.contains("XXX")]
-        return grouped_base, country_stats
+        # prep population data
+        df_wpp = self.wpp.preprocess_wpp(df_wpp)
+        df_wpp["quarter"] = [[1, 2, 3, 4] for i in range(len(df_wpp))]
+        df_wpp = df_wpp.explode("quarter")
+        df_wpp = df_wpp.set_index("quarter", append=True).sort_index()
+        df_wpp.loc[(slice(None), slice(None), slice(1, 3)), slice(None)] = np.nan
+        df_wpp = default_impute(df_wpp, location_index="iso3")
+        return grouped_base, country_stats, df_wpp
 
-    def create_indicator(self, dfs_preprocessed: tuple[pd.DataFrame, pd.DataFrame]) -> pd.DataFrame:
-        grouped_base, country_stats = dfs_preprocessed
+    def create_indicator(self, dfs_preprocessed: tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]) -> pd.DataFrame:
+        grouped_base, country_stats, df_wpp = dfs_preprocessed
         # event threshold for actors currently set to 0, could be revisited in the future
         event_threshold = 0
         grouped = (
@@ -47,8 +59,10 @@ class ConContextActors(Indicator, NormalizationMixin):
             .count()
         )
         grouped = pd.merge(grouped, country_stats, how="left", on=["iso3", "year", "quarter"])
-        grouped[self.composite_id] = grouped["actor"].apply(np.log1p) * grouped["best"].apply(
-            np.log1p
+        grouped = pd.merge(grouped, df_wpp, how="left", on=["iso3", "year", "quarter"])
+        grouped[self.composite_id] = (
+            grouped["actor"].apply(np.log1p) * grouped["best"].apply(np.log1p) /
+            grouped["pop_total"].apply(np.log1p)
         )
         # produce some additional history for normalization
         df_base = self.create_base_df(self.global_config["start_year"] - 3)
