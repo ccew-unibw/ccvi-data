9c0c647006d21f023c23524759d185273cc197a9
DaMitti <da.mitti@posteo.de>
2025-09-03 14:35:49 +0000

    set bii file location to repo version, remove from input config

--- base/datasets/bii.py
+++ base/datasets/bii.py
@@ -6,14 +6,17 @@ from base.objects import Dataset
 class BIIData(Dataset):
     """Handles loading and preprocessing of Magpie BII data.
 
-    Implements `load_data()` to load the magpie bii data from input storage.
+    Implements `load_data()` to load the magpie bii data from the repo.
 
     Attributes:
         data_key (str): Set to "bii".
+        local (bool): Set to False, since data is published in this repo and does
+            not need to be added to input data manually.
     """
 
     data_key: str = "bii"
+    local: bool = False
 
     def load_data(self) -> xr.Dataset:
         """Loads and returns data from input storage"""
-        return xr.open_dataset(self.data_config[self.data_key], decode_times=False)
+        return xr.open_dataset("./bii_ccvi/cell.bii_0.5.nc", decode_times=False)
